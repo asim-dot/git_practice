8e04e651904b668a2630de620fc0862f5b5c8f20
your <masim227@gmail.com>
2025-12-18 20:23:50 +0530

    new changes for CI pipeline

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ print('feature-1 added')
 print('saving feature-3')
 print('saving feature-5')
 print('pulling the code')
+
+def add(x, y):
+    return x + y
+
+if __name__ == "__main__":
+    print(add(2, 3))
